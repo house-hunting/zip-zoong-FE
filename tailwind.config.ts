2a3957736a2e6ace8055ec88d32a997ab84fd722
seonyeong719 <sy904280@gmail.com>
2023-11-24 15:57:18 +0900

    :lipstick: Design: 이미지 추가,폴더 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,59 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: "600px",
+      tablet: "800px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: "#5FCDFC",
+          200: "#0BB3FB",
+        },
+        subColor: `#FDF3D8`,
+        gray: {
+          100: "#E9E9E9",
+          200: "#D9D9D9",
+          300: "#656565",
+        },
+        hover: "#178AF4",
+        font: {
+          gray100: "#646F7C",
+          gray200: "#808080",
+          error: "#FF0000",
+          success: "#02B62D",
+          black: "#000000",
+          white: "#FFFFFF",
+        },
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        md: "1.25rem",
+        big: "1.5rem",
+        lg: "1.75rem",
+        xl: "2rem",
+      },
+      fontWeight: {
+        light: "300",
+        regular: "400",
+        bold: "700",
+        extraBold: "900",
+      },
+      width: {
+        "10": "10rem",
+        "25": "25rem",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

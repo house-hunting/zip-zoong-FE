faff8ae8c26c5f93e9fb74b0b82afb7ade63e5cb
seonyeong719 <sy904280@gmail.com>
2023-12-02 21:59:32 +0900

    :lipstick: Design: soldOutCard css 추가

---
+++ src/components/Card/SoldOutCard.tsx
@@ -0,0 +1,8 @@
+export const SoldOutCard = () => {
+  return (
+    <>
+      <div className=" backdrop-blur-sm absolute h-full w-full "></div>
+      <div className="top-1/3 absolute text-gray-300 text-lg font-extraBold text">거래완료</div>
+    </>
+  );
+};

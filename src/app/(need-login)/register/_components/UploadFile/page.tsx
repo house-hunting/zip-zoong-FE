727d89ab3c23b67ed6f8c5b6be0bc5b426765fdf
seonyeong719 <sy904280@gmail.com>
2023-12-12 00:11:18 +0900

    :lipstick: Design: uploadfile css

---
+++ src/app/(need-login)/register/_components/UploadFile/page.tsx
@@ -0,0 +1,16 @@
+export default function UploadFile() {
+  return (
+    <div className="border grid grid-cols-6 justify-between">
+      <div className="flex justify-center items-center border">
+        <div>사진 등록</div>
+      </div>
+      <div className="col-span-2 p-5">
+        <label htmlFor="fileInput" className="cursor-pointer p-2 rounded-md bg-black text-white">
+          + 사진 추가
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+        </label>
+      </div>
+      <div className="flex justify-center items-center"></div>
+    </div>
+  );
+}

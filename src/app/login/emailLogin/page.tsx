204dfc42a8babd2d8b398d30602009127d87c574
seonyeong719 <sy904280@gmail.com>
2023-12-13 02:39:08 +0900

    :sparkles: Feat: 로그인 훅폼

--- src/app/login/emailLogin/page.tsx
+++ src/app/login/emailLogin/page.tsx
@@ -3,25 +3,73 @@
 import Image from "next/image";
 import logo from "/public/집중 메인로고.png";
 import { useRouter } from "next/navigation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { LoginInput } from "./_components/loginInput";
+
+const LoginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("이메일을 필수로 입력해주세요")
+    .email("이메일 형식에 맞게 입력해주세요"),
+  password: yup
+    .string()
+    .required("비밀번호는 필수 입력입니다.")
+    .min(8, "최소8자 이상 입력해주세요")
+    .max(18, "18자 이내로 입력해주세요")
+    .matches(
+      /^.*(?=^.{8,18}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/,
+      "특수문자, 문자, 숫자를 포함한 형태의 암호를 입력해 주세요"
+    ),
+});
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export default function Login() {
   const router = useRouter();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(LoginSchema) });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    console.log(data);
+  };
 
   const handleClick = () => {
     router.push("/signUp");
   };
 
   return (
-    <form className="flex justify-center items-center h-screen">
+    <form className="flex justify-center items-center h-screen" onSubmit={handleSubmit(onSubmit)}>
       <div className="border w-25 flex justify-center items-center rounded-md p-5 flex-col px-9">
         <Image className=" w-1/2 py-2" src={logo} alt="logo" />
         <div className="flex flex-col items-center text-md py-2">
           <span>간편하게 로그인 하고</span>
           <span className=" font-extraBold">다양한 서비스를 즐기세요.</span>
         </div>
-        <div className="py-4">
-          <input className="border w-full p-3 my-2" placeholder="이메일 주소" type="text" />
-          <input className="border w-full p-3 my-2" placeholder="비밀번호" type="password" />
+        <div className="py-4 w-full">
+          <LoginInput
+            name="email"
+            type="text"
+            control={control}
+            errors={errors}
+            placeholder="이메일"
+          />
+          <LoginInput
+            name="password"
+            type="password"
+            control={control}
+            errors={errors}
+            placeholder="비밀번호"
+          />
         </div>
         <div className="w-full flex justify-center items-center text-xs text-font-gray200">
           <span className="mx-3 font-bold cursor-grab">아이디 찾기</span>
